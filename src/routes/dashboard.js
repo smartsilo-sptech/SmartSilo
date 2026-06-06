98efe1b46fddca4ab3f00e06f03f24bc9acfba60
Jefferson <jefferson.watabe@sptech.school>
2026-06-06 01:34:04 -0300

    iniciando a criação das rotas para dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-var medidaController = require("../controllers/medidaController");
+var dashboardController = require("../controllers/dashboardController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
+router.get("/visao-geral", function(req, res) {
+    dashboardController.buscarVisaoGeral(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
+router.get("/panoramica", function(req, res) {
+    dashboardController.buscarPanoramica(req, res);
+});
+
+router.get("/alertas", function(req, res) {
+    dashboardController.buscarAlertas(req, res);
+});
 
 module.exports = router;
